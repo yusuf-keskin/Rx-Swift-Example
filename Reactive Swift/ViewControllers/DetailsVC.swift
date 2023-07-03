b08d8d5602120e6cfc51409910ff14863f939f23
Yusuf Keskin <yusuf_keskin@outlook.com>
2023-07-03 22:33:26 +0300

    Project Start

---
+++ Reactive Swift/ViewControllers/DetailsVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsVC.swift
+//  Reactive Swift
+//
+//  Created by YUSUF KESKİN on 3.07.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
